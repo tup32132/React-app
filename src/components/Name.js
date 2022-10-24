769dcef47c3311bab353331116b9b8b71bd1c40b
Kyle Kinney <tup32132@temple.edu>
2022-10-24 15:19:22 -0400

    workshopped tutorial code w/ Josh

---
+++ src/components/Name.js
@@ -0,0 +1,14 @@
+//import React from 'react';
+
+const Kyle = (props) => {
+    //const name = 'Kyle'
+    return(
+    <> 
+    <h1> {props.name} </h1>
+            <p> {props.text} </p>
+    </>
+    )
+
+}
+
+export default Kyle
